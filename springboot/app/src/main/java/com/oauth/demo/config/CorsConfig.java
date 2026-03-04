064f5236c5405f44b97f422753993ee5996081b3
sadokjeridi <sedakjriidi@gmail.com>
2026-03-04 14:51:22 +0100

    fix page

--- springboot/app/src/main/java/com/oauth/demo/config/CorsConfig.java
+++ springboot/app/src/main/java/com/oauth/demo/config/CorsConfig.java
@@ -1,8 +1,11 @@
 package com.oauth.demo.config;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@CrossOrigin(origins = {"http://localhost:4200/", "http://127.0.0.1:4200"})
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
