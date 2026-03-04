3293a9acdb9d31ed15bc6c6931dd24edfed35bda
sadokjeridi <sedakjriidi@gmail.com>
2026-03-04 15:30:03 +0100

    Enable CORS for Angular frontend on UserController

--- springboot/app/src/main/java/com/oauth/demo/controllers/UserController.java
+++ springboot/app/src/main/java/com/oauth/demo/controllers/UserController.java
@@ -1,12 +1,12 @@
 package com.oauth.demo.controllers;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import com.oauth.demo.entities.User;
 import com.oauth.demo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = {"http://localhost:4200/", "http://127.0.0.1:4200"})
 @RestController
 @RequestMapping(value = "/user")
 public class UserController {
